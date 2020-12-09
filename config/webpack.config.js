118ede8277ad378e3193821e07a5388e615f49ea
brant <403216075@qq.com>
2020-12-09 20:02:09 +0800

    [feat]添加打包字体文件

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "build.bundle.js",
+    publicPath: "./",
     path: resolve(__dirname, "../build"),
   },
   module: {
@@ -17,6 +18,27 @@ module.exports = {
       {
         test: /\.less$/,
         use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: "url-loader",
+        options: {
+          limit: 8 * 1024,
+          name: "[hash:10].[ext]",
+        },
+      },
+      {
+        test: /\.html$/,
+        // 处理html中的img图片
+        loader: "html-loader",
+      },
+      {
+        test: /\.(eot|ttf|svg|woff|woff2)$/,
+        // 处理html中的img图片
+        loader: "file-loader",
+        options: {
+          name: "[hash:10].[ext]",
+        },
       },
     ],
   },
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import "./index.css";
 import "./index.less";
+import "./iconfont.css";
 const a = 12;
 const b = 1;
 console.log(a + b);
