e1066be58521e07149fecc4375495bd9d37d6301
brant <403216075@qq.com>
2020-12-11 12:16:42 +0800

    [feat]添加eslint校验

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,17 +1,20 @@
-const { resolve } = require("path");
+const { resolve } = require('path');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 // 指定node环境变量   让postcss对package.json中browserslist的不同环境做处理
 // process.env.NODE_ENV = "development";
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "build.bundle.js",
-    publicPath: "./",
-    path: resolve(__dirname, "../build"),
+    filename: 'build.bundle.js',
+    publicPath: './',
+    path: resolve(__dirname, '../build'),
   },
   module: {
     rules: [
@@ -22,7 +25,7 @@ module.exports = {
           // "style-loader",
           MiniCssExtractPlugin.loader,
           // 将css文件整合到js中
-          "css-loader",
+          'css-loader',
         ],
       },
       {
@@ -30,14 +33,14 @@ module.exports = {
         use: [
           // "style-loader",
           MiniCssExtractPlugin.loader,
-          "css-loader",
+          'css-loader',
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               postcssOptions: {
                 plugins: [
                   [
-                    "postcss-preset-env",
+                    'postcss-preset-env',
                     {
                       // 其他选项
                     },
@@ -46,46 +49,66 @@ module.exports = {
               },
             },
           },
-          "less-loader",
+          'less-loader',
         ],
       },
       {
         test: /\.(jpg|png|gif)$/,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
           limit: 8 * 1024,
-          name: "[hash:10].[ext]",
+          name: '[hash:10].[ext]',
         },
       },
       {
         test: /\.html$/,
         // 处理html中的img图片
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.(eot|ttf|svg|woff|woff2)$/,
         // 处理html中的img图片
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[hash:10].[ext]",
+          name: '[hash:10].[ext]',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "css/index.css",
+      filename: 'css/index.css',
     }),
     new OptimizeCssAssetsWebpackPlugin(),
+    new EslintWebpackPlugin({
+      // Plugin options
+      extensions: ['js', 'mjs', 'jsx', 'ts', 'tsx'],
+      // formatter: require.resolve('react-dev-utils/eslintFormatter'),
+      // eslintPath: require.resolve('eslint'),
+      // context: paths.appSrc,
+      // cache: true,
+      // ESLint class options
+      // cwd: paths.appPath,
+      // resolvePluginsRelativeTo: __dirname,
+      // baseConfig: {
+      //   extends: [require.resolve('eslint-config-react-app/base')],
+      //   rules: {
+      //     ...(!hasJsxRuntime && {
+      //       'react/react-in-jsx-scope': 'error',
+      //     }),
+      //   },
+      // },
+    }),
+    new CleanWebpackPlugin(),
   ],
-  mode: "development",
+  mode: 'development',
   //   mode: "production",
 
   devServer: {
-    contentBase: resolve(__dirname, "build"),
+    contentBase: resolve(__dirname, 'build'),
     // 启动gzip压缩
     compress: true,
     port: 3000,
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import "./index.css";
-import "./index.less";
-import "./iconfont.css";
+import './index.less';
+import './iconfont.css';
 const a = 12;
 const b = 1;
+
 console.log(a + b);
