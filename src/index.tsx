d0f34ec738a8af7de205cd0e28d7ef0915db6d99
brant <403216075@qq.com>
2020-12-21 17:10:37 +0800

    [feat]添加js热更新

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 // 局部热更新
-// if (module?.hot) {
-//   module.hot.accept();
-// }
+if (module?.hot) {
+  module.hot.accept();
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
