d49f2b132093f49f485da5ddc13e57e5f874a12d
RThong <403216075@qq.com>
2021-01-19 23:10:40 +0800

    [feat]添加打包分析

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server', // 开一个本地服务查看报告
+      analyzerHost: '127.0.0.1', // host 设置
+      analyzerPort: 8888, // 端口号设置
+    }),
+  ],
 });
